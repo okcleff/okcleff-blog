753cf90dd67a97f8ea62fcdf357f1e5cb2206ef7
okcleff <okcleff@gmail.com>
2023-03-07 11:34:57 +0900

    Modify: AuthorLayout 수정

--- layouts/MDX/AuthorLayout.tsx
+++ layouts/MDX/AuthorLayout.tsx
@@ -1,4 +1,3 @@
-import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
 import type { Authors } from 'contentlayer/generated'
 import { ReactNode } from 'react'
@@ -9,7 +8,7 @@ interface Props {
 }
 
 export default function AuthorLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company } = content
+  const { name, occupation, company } = content
 
   return (
     <>
@@ -17,19 +16,10 @@ export default function AuthorLayout({ children, content }: Props) {
       <div className="pt-8">
         <div className="mb-8 flex flex-col-reverse items-center justify-between sm:flex-row sm:items-center">
           <div className="text-center sm:text-left">
-            <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">Dale Larroder</h1>
+            <h1 className="text-xl font-bold md:text-3xl lg:text-4xl">Woncheol Ok</h1>
             <h2 className="text-sm font-normal md:text-base">
               {occupation} <span className="font-semibold">{company}</span>
             </h2>
-          </div>
-          <div>
-            <Image
-              alt="Dale Larroder"
-              height={130}
-              width={130}
-              src={avatar || ''}
-              className="rounded-full object-scale-down grayscale"
-            />
           </div>
         </div>
         <div className="prose max-w-none pb-8 text-justify text-sm dark:prose-dark md:text-lg xl:col-span-2">
