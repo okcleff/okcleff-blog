d08c527eb4c95316a564aab5aae94f9eed50e0c3
okcleff <okcleff@gmail.com>
2023-03-07 14:14:52 +0900

    Modify: config 수정

--- next.config.js
+++ next.config.js
@@ -62,8 +62,7 @@ const securityHeaders = [
 module.exports = withContentlayer(
   withBundleAnalyzer({
     reactStrictMode: true,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    // pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
       dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
     },
@@ -85,27 +84,3 @@ module.exports = withContentlayer(
     },
   })
 )
-
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-})
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configure pageExtensions to include md and mdx
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  // Optionally, add any other Next.js config below
-  reactStrictMode: true,
-}
-
-// Merge MDX config with Next.js config
-module.exports = withMDX(nextConfig)
