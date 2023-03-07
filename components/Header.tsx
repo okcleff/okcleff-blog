4394dc0951682bcbcc58b0165f01a14c06d53781
okcleff <okcleff@gmail.com>
2023-03-07 11:49:05 +0900

    Modify: 헤더 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
                 }
               )}
             >
-              d.
+              okcleff
             </div>
           </Link>
         </div>
